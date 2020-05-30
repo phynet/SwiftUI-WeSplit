c457a38711360345b573240a08f487d3fe8a251e
Sofía Swidarowicz Andrade <sofia.swidarowicz@gmail.com>
2020-05-30 19:48:48 +0200

    Binding state to user interface controls

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -9,10 +9,11 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var tapCount = 0
+    @State var name = ""
     var body: some View {
-        Button("Tap Count \(tapCount)") {
-            self.tapCount += 1
+        Form {
+            TextField("Enter your name:", text: $name)
+            Text("Your name is: \(name)")
         }
     }
 }
