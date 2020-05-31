1a13bb69d529c54074a4eb5a9bac2abbd9e99ef6
Sofía Swidarowicz Andrade <sofia.swidarowicz@gmail.com>
2020-05-31 12:29:43 +0200

    Reading text from the user with TextField

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -9,17 +9,19 @@
 import SwiftUI
 
 struct ContentView: View {
-    let students = ["Harry", "Hermione", "Ron"]
-    @State private var selectedStudent = 0
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 2
+    let tipPercentages = [10, 15, 20, 25, 0]
 
     var body: some View {
-        VStack {
-            Picker("Select your student", selection: $selectedStudent) {
-                ForEach(0 ..< students.count) {
-                    Text(self.students[$0])
-                }
+        Form {
+            Section {
+                TextField("Amount", text: $checkAmount)
             }
-            Text("You chose: Student # \(students[selectedStudent])")
+            Section {
+                Text("$\(checkAmount)").keyboardType(.decimalPad)
+            }
         }
     }
 }
